226307ac6ea9495bbcf469725fe3c66f58e54409
Daan2001 <nautssyndicate@gmail.com>
2021-04-23 20:06:26 +0200

    Restructured some functions

--- Webpage/Webpagecode.js
+++ Webpage/Webpagecode.js
@@ -1,22 +1,3 @@
-//A simple test function, hence, no documentation.
-//Simply for the sake of playing around with D3 and JS.
-function tempfunction() {
-    LogData("Dataset.csv");
-
-    d3.select("svg").append("rect").attr("x",150).attr("y", 200).attr("width", 50).attr("height", 140).attr('stroke', 'black')
-        .attr('fill', '#69a3b2');
-    
-}
-
-//A function which logs the first entry of a CSV dataset into the console. 
-//Parameter: Datapath = string, path to the deserived file.
-function LogData(dataPath) {
-    d3.csv(dataPath).then(function(data) {
-        //Since d3.csv is asynchronous (it is not loaded immediatly, but it is a request to the webserver) we need all our code from the data in here. 
-        console.log(data[0]);
-      });
-}
-
 //A function that toggles specific classes and the visibility of the text elements in the sidebar.
 //Parameter: none
 function toggleSidebar() {
