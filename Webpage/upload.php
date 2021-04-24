081eb97454428870cf5becbfe8670100dc77bc59
MikevdAnker <mikevdanker@gmail.com>
2021-04-24 16:48:34 +0200

    Error handling for the file upload in php got added (not yet tested, except for filetype

--- Webpage/upload.php
+++ Webpage/upload.php
@@ -15,9 +15,6 @@ if (isset($_POST['submit'])) {
     $fileExtRaw = explode("." , $fileName);
     $fileExt = strtolower(end($fileExtRaw));
 
-    #allowed is an array consisting of the allowed data types:
-    $allowed = array("csv");
-
     #Execute the check:
     if (in_array($fileExt, $allowed)) {
         #check for general error:
@@ -32,12 +29,32 @@ if (isset($_POST['submit'])) {
             #return to html page:
             header("location: dbl_vis.html");
 
-        } else {
-            echo "There was an error uploading your file: ".$fileError;
+        } else if ($fileError === 1){
+            #File size error (from php.ini)
+            echo '<script type="text/javascript">alert("The uploaded file exceeded the maximum file size."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 2){
+            #File size error (from HTML form)
+            echo '<script type="text/javascript">alert("The uploaded file exceeded the maximum file size."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 3){
+            #File partial upload error
+            echo '<script type="text/javascript">alert("The uploaded file was only partially uploaded."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 4){
+            #File missing error
+            echo '<script type="text/javascript">alert("No file was uploaded."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 6){
+            #Missing folder error
+            echo '<script type="text/javascript">alert("Missing a temporary folder."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 7){
+            #Disk error
+            echo '<script type="text/javascript">alert("Failed to write file to disk."); window.location.href = "dbl_vis.html";</script>';
+        } else if ($fileError === 8){
+            #PHP error
+            echo '<script type="text/javascript">alert("A PHP extension stopped the file upload."); window.location.href = "dbl_vis.html";</script>';
         }
 
     } else { 
-        echo "This type of file is not allowed.";
+        #File type error [TESTED]
+        echo '<script type="text/javascript">alert("This type of file is not allowed."); window.location.href = "dbl_vis.html";</script>';
     }
 
 }
