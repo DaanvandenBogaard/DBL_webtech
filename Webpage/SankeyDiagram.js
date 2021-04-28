84df222f70fb7114a99340c1624a5e627e2a972b
Daan2001 <nautssyndicate@gmail.com>
2021-04-28 14:14:06 +0200

    changed the MakeSankey function

--- Webpage/SankeyDiagram.js
+++ Webpage/SankeyDiagram.js
@@ -3,14 +3,33 @@ Daan van den Bogaard (1534173)
 Myrte van Ginkel (1566237)
 */
 
-//Import sankey package as variable:
-var sankey = d3.sankey();
-
+//TODO: update hardcoded parameters
 function makeSankey(dataPath) {
-    d3.csv(dataPath).then(function(data) {
+
+  //We start by making the SVG element.
+  let margin = {top : 15, right : 10, bottom: 15, left: 10} //For now, hardcoded margins 
+  let width = 500; //for now, hardcoded width
+  let height = 500; //for now, hardcoded height
+  let div = d3.select("#sankeyID");
+  let svg = div.append("svg")
+               .attr("width" , width)
+               .attr("height", height)
+               .append("g")
+               .attr("transform" , "translate(" + margin.left  + "," + margin.top +")");
+  //Import sankey package as variable:
+  let sankey = d3.sankey()
+               .nodeWidth(40)
+               .nodePadding(300)
+               .size([width,height]);
+              
+  //Choose a color scale:
+  let colorScale = d3.scaleOrdinal("d3.schemeCategory10");
+
+  //From now on, we load the data:
+  d3.csv(dataPath).then(function(data) {
         //Since d3.csv is asynchronous (it is not loaded immediatly, but it is a request to the webserver) we need all our code from the data in here. 
       
-
+      
         
-      });
+  });
 }
