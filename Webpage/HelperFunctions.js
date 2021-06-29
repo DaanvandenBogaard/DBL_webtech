ab88a78dbee5198fe9a2884dc097cdfe5f7f49bd
Daan2001 <nautssyndicate@gmail.com>
2021-06-29 22:11:23 +0200

    Deleted folder with duplicate code

--- Webpage/HelperFunctions.js
+++
@@ -1,8 +0,0 @@
-//A function which logs the first entry of a CSV dataset into the console. 
-//Parameter: Datapath = string, path to the deserived file.
-function LogData(dataPath) {
-  d3.csv(dataPath).then(function(data) {
-    //Since d3.csv is asynchronous (it is not loaded immediatly, but it is a request to the webserver) we need all our code from the data in here. 
-    console.log(data[0]);
-  });
-}
